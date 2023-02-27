4149406430872383cd780b5aa196c48982ab4be6
Sakinah Freeman <freemansakinah@gmail.com>
2023-02-26 19:21:38 -0500

    displayed search bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,16 @@
-const Nav = (props) => {
+import { Link } from "react-router-dom"
 
+const Nav = (props) => {
+    return (
+        <div className="nav">
+            <Link to="/">
+                <span>Recipe Journal</span>
+            </Link>
+            <Link to="create">
+                <span>New</span>
+            </Link>
+        </div>
+    )
 }
 
 export default Nav
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,25 @@
-const SearchBar = (props) => {
+import { useState } from "react"
 
+const SearchBar = (props) => {
+    const [formData, setFormData] = useState({searchTerm: ""})
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.recipeSearch(formData.searchTerm)
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="searchTerm" value={formData.searchTerm} onChange={handleChange}/>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    )
 }
 
 export default SearchBar
