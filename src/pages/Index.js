371fbd74c4989ee9c9aad58eaf74e2d3a1217750
Sakinah Freeman <freemansakinah@gmail.com>
2023-02-27 20:31:05 -0500

    some more styling

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-import Post from "../components/Post";
-import SearchBar from "../components/SearchBar";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Index = (props) => {
     const recipes = useLoaderData()
+    console.log("LOADED DATA:", recipes)
+
+    const [search, setSearch] = useState("")
+    console.log(recipes.filter((recipe) => recipe.name.toLowerCase().includes("search")))
     
     return <>
-        <SearchBar data={recipes}/>
         <div className="recipes">
-        {/* {recipes.map((post) => <Post key={post.id} post={post} />)} */}
+            <div className="searchBar">
+                <input type="text" placeholder="Search.." className="search" onChange={event => setSearch(event.target.value)}/>
+                <ul className="search-list">
+                    {recipes.filter(recipe => recipe.name.toLowerCase().includes(search)).map((recipe) => (
+                    <Link to={`/post/${recipe.id}`}>
+                        <li key={recipe.id} className="list-item">{recipe.name}</li>
+                    </Link>
+                    ))}
+                </ul>
+            </div>
         </div>
     </>
   }
